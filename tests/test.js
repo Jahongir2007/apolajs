c4e3421bdbfed700523b5afe5a9c6b31efd99857
Jahongir Sobirov <69193276+Jahongir2007@users.noreply.github.com>
2021-07-12 21:58:42 +0500

    "Hello, world!" application in Apola.js

---
+++ tests/test.js
@@ -0,0 +1,10 @@
+const apola = require('apola');
+var app = apola;
+
+app.route(['/api/test/method/route'], {
+  echo: ["Hello, world!", {
+    status: false
+  }]
+});
+
+app.port(3000, "HTTP server created!!!");
